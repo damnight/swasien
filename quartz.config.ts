b840ee394af0b8324d28876556b76a74b194057a
Lucy Dust <lucrezia.d.ast@protonmail.com>
2026-02-20 23:25:07 +0100

    Wiki update: 2026-02-20 23:25

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "damnight.github.io/Swasien",
+    baseUrl: "damnight.github.io/swasien",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
